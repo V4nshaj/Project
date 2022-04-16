e1d2494d2cbea22f76f100e86db6486d277ed302
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2022-04-16 21:13:21 +0530

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,6 +1,6 @@
 print("Welcome to the payment calculator!")
 bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10%, 12%, or 15% of the bill? "))
+tip = int(input("How much tip would you like to give? 0%, 10%, 12%, or 15% of the bill? "))
 people = int(input("How many people to split the bill?"))
 tip_per=bill*tip/100
 each=(bill+tip_per)/people

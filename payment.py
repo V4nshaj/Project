95bf4e346d9200fd1ab44818e81a53aae76e725c
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2022-04-16 21:12:22 +0530

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,9 +1,7 @@
-print("Welcome to the tip calculator!")
+print("Welcome to the payment calculator!")
 bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+tip = int(input("How much tip would you like to give? 10%, 12%, or 15% of the bill? "))
 people = int(input("How many people to split the bill?"))
 tip_per=bill*tip/100
 each=(bill+tip_per)/people
-print("Each person should pay: " + "{0:.2f}".format(each))#to print till 2 decimal
-print("Each person should pay: ", round(each,2))#to print till 2 decimal
-print("Each person should pay: " + "%.2f"%each)#to print till 2 decimal
+print("Each person should pay: " + "%.2f"%each)
